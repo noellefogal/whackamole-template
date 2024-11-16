692a60dc43d56aa3a768eadd91b16a627557e37a
noellefogal <noellefogal@ufl.edu>
2024-11-16 03:17:07 -0500

    started project

--- whackamole.py
+++ whackamole.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # make a comment
 def main():
@@ -14,7 +15,11 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    print(event.pos)
             screen.fill("light green")
+            screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0)))
+            pygame.draw.line(screen, "dark blue", (32, 0), (32, 512))
             pygame.display.flip()
             clock.tick(60)
     finally:
